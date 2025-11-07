39474d3698dea5e6d69ed9969f828f0f0be276a9
anton <antontat27@gmail.com>
2025-11-07 17:31:57 +0200

    Added colours

--- client.py
+++ client.py
@@ -3,4 +3,7 @@ import requests
 # Sending a post request to the server (API)
 response = requests.get(url="http://127.0.0.1:8080/")
 # Printing out the response of API
-print(response.text)
+if response.status_code == 200:
+    print("Success, output: ", response.text)
+else:
+    print("Error")
